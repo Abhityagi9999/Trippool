c86195c06ba84d0c4ef66ff5433be4e0e53a95b6
Abhi <abhityagi23002@gmail.com>
2026-03-27 07:47:27 -0700

    Branding Polish v1.17: Serving sw-v17.js and finalized user logo

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def robots(): return send_from_directory('static', 'robots.txt')
 def sitemap(): return send_from_directory('static', 'sitemap.xml')
 
 @app.route('/sw.js')
-def sw(): return send_from_directory('static', 'sw-v16.js', mimetype='application/javascript')
+def sw(): return send_from_directory('static', 'sw-v17.js', mimetype='application/javascript')
 
 @app.route('/manifest.json')
 def manifest(): return send_from_directory('static', 'manifest.json')
